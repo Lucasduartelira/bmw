778336ec2a7c29ab077bd98152096664d5a4da47
Lucas Duarte Lira <lucasduartelira@gmail.com>
2026-05-10 12:42:46 -0300

    color-options

---
+++ main.js
@@ -0,0 +1,15 @@
+const radios = document.querySelectorAll('input[name="car-color"]');
+const carImage = document.getElementById('carImage');
+
+radios.forEach((radio) => {
+  radio.addEventListener('change', () => {
+    // fade out
+    carImage.style.opacity = '0';
+
+    setTimeout(() => {
+      carImage.src = radio.dataset.img;
+      // fade in
+      carImage.style.opacity = '1';
+    }, 300);
+  });
+});
